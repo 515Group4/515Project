b6588bdffe50892bfe57da265b317cfc5f3cde65
Sushovan De <sushovande@yahoo.co.in>
2010-11-04 23:52:18 -0700

    Fixed Thumbs.db bug, added code to rename files to integer values as required by our index structure

--- Phase 2/ResizePictures/Program.cs
+++ Phase 2/ResizePictures/Program.cs
@@ -4,6 +4,12 @@ using System.Text;
 using System.Drawing;
 using System.IO;
 
+// to run this program -- 
+// Step 1. set the FolderName
+// Step 2. set the doRename variable to 1. Run the program.
+// Step 3. set the doRename variable to 2. Run the program.
+// Step 4. set the doRename variable to 3. Run the program.
+
 namespace ResizePictures
 {
     class Program
@@ -12,30 +18,63 @@ namespace ResizePictures
         {
             const string FolderName = @"C:\Data\Datasets\t2";
             string[] files = Directory.GetFiles(FolderName);
-            foreach (var filename in files)
+            
+            int doRename = 3;
+
+            // rename the files to numbers
+            if (doRename == 2)
             {
-                if (filename.EndsWith("Thumbs.db"))
+                int i = 0;
+                foreach (var filename in files)
                 {
-                    continue;
-                }
-                Console.WriteLine(filename);
-                Bitmap b = (Bitmap)Bitmap.FromFile(filename);
-                if (b.Width > 1000 || b.Height > 1000)
-                {
-                    double fx = 600 / (double)b.Width;
-                    double fy = 600 / (double)b.Height;
-                    double f = Math.Min(fx, fy);
-
-                    int reducedWidth = (int)(f * b.Width);
-                    int reducedHeight = (int)(f * b.Height);
-                    Bitmap b2 = new Bitmap(reducedWidth, reducedHeight);
-                    using (Graphics g = Graphics.FromImage(b2))
+                    if (filename.EndsWith("Thumbs.db"))
                     {
-                        g.DrawImage(b, new Rectangle(0, 0, reducedWidth, reducedHeight));
+                        continue;
                     }
+                    i++;
+                    File.Move(filename, Path.Combine(Path.GetDirectoryName(filename), "foo__" + i + ".tif"));
+                }
+            }
+            else if (doRename == 3)
+            {
+                int i = 0;
+                foreach (var filename in files)
+                {
+                    if (filename.EndsWith("Thumbs.db"))
+                    {
+                        continue;
+                    }
+                    i++;
+                    File.Move(filename, Path.Combine(Path.GetDirectoryName(filename), i + ".tif"));
+                }
+            }
+            else // resize the files
+            {
+                foreach (var filename in files)
+                {
+                    if (filename.EndsWith("Thumbs.db"))
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(filename);
+                    Bitmap b = (Bitmap)Bitmap.FromFile(filename);
+                    if (b.Width > 1000 || b.Height > 1000)
+                    {
+                        double fx = 600 / (double)b.Width;
+                        double fy = 600 / (double)b.Height;
+                        double f = Math.Min(fx, fy);
 
-                    b.Dispose();
-                    b2.Save(filename, System.Drawing.Imaging.ImageFormat.Tiff);
+                        int reducedWidth = (int)(f * b.Width);
+                        int reducedHeight = (int)(f * b.Height);
+                        Bitmap b2 = new Bitmap(reducedWidth, reducedHeight);
+                        using (Graphics g = Graphics.FromImage(b2))
+                        {
+                            g.DrawImage(b, new Rectangle(0, 0, reducedWidth, reducedHeight));
+                        }
+
+                        b.Dispose();
+                        b2.Save(filename, System.Drawing.Imaging.ImageFormat.Tiff);
+                    }
                 }
             }
         }
