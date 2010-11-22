af6e5da08d776c3903346e15a20009be4ea2e54c
Rick Strong <rjstrong@gmail.com>
2010-11-21 17:59:54 -0700

    start of nearest neighbor interface

---
+++ phase3/NearestN/NearestN/IndexStub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NearestN
+{
+    interface IndexFile
+    {
+        // This method sets the index file we want to use (i.e., which feature)
+        // I think this can just be thrown in the constructor but i'm not sure how to do that
+        // with an interface
+        void pickIndex(String filename);
+
+        // Returns the next matched file (which I believe we represent as integers)
+        int getNextMatch();
+    }
+}
---
+++ phase3/NearestN/NearestN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NearestN
+{
+    class NearestN : IndexFile
+    {
+        private String file;
+        public void pickIndex(String i)
+        {
+            file = i;
+        }
+
+        public int getNextMatch()
+        {
+            return 1; // will eventually be random
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("TEST TEST TEST");
+        }
+    }
+}
